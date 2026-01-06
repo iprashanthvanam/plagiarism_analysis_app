e6f5cc89cf4454b67f71e79ca31747b4f1d6194a
iprashanthvanam <prashanthvanamnetha@gmail.com>
2026-01-06 22:19:45 +0530

    Change API base URL to production server

--- frontend/src/lib/api.tsx
+++ frontend/src/lib/api.tsx
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://plagiarism-analysis-app.onrender.com",
   withCredentials: true, // ✅ SEND COOKIES
 });
 

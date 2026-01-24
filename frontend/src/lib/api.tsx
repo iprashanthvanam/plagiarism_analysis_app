36ab85717c0212474fb057ca54480db9a6e56c64
iprashanthvanam <prashanthvanamnetha@gmail.com>
2026-01-25 00:26:09 +0530

    Update api.tsx

--- frontend/src/lib/api.tsx
+++ frontend/src/lib/api.tsx
@@ -1,12 +1,5 @@
 import axios from "axios";
-
 const api = axios.create({
-  // 👇 REPLACE THIS LINE
-  // baseURL: "https://plagiarism-analysis-app.onrender.com", 
-  // with this:
-  baseURL: "/api",
-
-  withCredentials: true, // ✅ SEND COOKIES
+baseURL: "https://plagiarism-analysis-app.onrender.com",
+withCredentials: true, // ✅ SEND COOKIES
 });
-
-export default api;
